f7a813c39a3b824dbd95b586702fde33f02ddb41
liuyang <1773587692@qq.com>
2023-08-01 14:26:55 +0800

    feat: 使用express应用处理响应

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { AdminControllerController } from './admin-controller/admin-controller.controller';
 import { ReCatsController } from './re-cats/re-cats.controller';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController, AdminControllerController, ReCatsController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
--- src/re-cats/re-cats.controller.ts
+++ src/re-cats/re-cats.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Post, Res, HttpStatus } from '@nestjs/common';
+import { Response} from 'express';
 @Controller('re-cats')
-export class ReCatsController {}
+export class ReCatsController {
+  @Post()
+  create(@Res() res:Response) {
+    res.status(HttpStatus.CREATED).send({
+      data: 'something'
+    })
+  }
+
+  @Get()
+  findAll(@Res({ passthrough: true }) res: Response) {
+    res.status(HttpStatus.OK);
+    return [1,2,3,4,5,'s'];
+  }
+}
